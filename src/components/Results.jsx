b1655683278c6768677a34f08c4ec6a56cfe7f04
weterynarzfred <weterynarzfred@gmail.com>
2021-06-07 19:18:24 +0200

    display some options in results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { optionTypes } from '../include/constants';
+import { isSelected } from '../functions/getSelectedValue';
+import { getUserValue } from '../include/userValues';
 
 function Results(props) {
+  const optionElements = Object.keys(props.options).map(optionKey => {
+    const option = props.options[optionKey];
+    if (![optionTypes.INTEGER, optionTypes.SLIDER].includes(option.type)) return;
+    if (!isSelected(option, props.options)) return;
+    return <div className="result-option" key={optionKey}>
+      <div className="result-option-key">{optionKey}</div>
+      <div className="result-option-title">{getUserValue(optionKey, 'title')}</div>
+    </div>;
+  });
+
   return <div className="Results">
-    <p>// TODO: Results will go here.</p>
+    {optionElements}
   </div>;
 }
 
 export default connect(state => {
-  return {};
+  return {
+    options: state.options,
+  };
 })(Results);
