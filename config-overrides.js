1c684b2b94eb911698eee8ee52725d30af2adb04
weterynarzfred <weterynarzfred@gmail.com>
2021-12-17 19:49:35 +0100

    debug build

--- config-overrides.js
+++ config-overrides.js
@@ -4,20 +4,20 @@ module.exports = function override(config) {
   // switch to example if content is not provided
   config.resolve.modules.push('src/content', 'src/example');
 
+  const BASE64_MAX_SIZE = 1000;
+
   // resize and convert images
   if (process.env.NODE_ENV === 'production') {
-    config.module.rules[1].oneOf.splice(
+    config.module.rules[0].oneOf.splice(
       1,
       1,
       {
-        test: /\.(bmp|jpe?g|gif|png)$/i,
+        test: /\.(bmp|jpe?g|gif|png|webp)$/i,
         resourceQuery: /large/,
         exclude: resolve(__dirname, './src/content/media/general'),
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: BASE64_MAX_SIZE } },
         use: [
-          {
-            loader: 'file-loader',
-            options: config.module.rules[1].oneOf[1].options,
-          },
           {
             loader: 'webpack-image-resize-loader',
             options: {
@@ -30,13 +30,11 @@ module.exports = function override(config) {
         ],
       },
       {
-        test: /\.(bmp|jpe?g|gif|png)$/i,
+        test: /\.(bmp|jpe?g|gif|png|webp)$/i,
         include: resolve(__dirname, './src/content/media/containers'),
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: BASE64_MAX_SIZE } },
         use: [
-          {
-            loader: 'file-loader',
-            options: config.module.rules[1].oneOf[1].options,
-          },
           {
             loader: 'webpack-image-resize-loader',
             options: {
@@ -48,22 +46,16 @@ module.exports = function override(config) {
         ],
       },
       {
-        test: /\.(bmp|jpe?g|gif|png)$/i,
+        test: /\.(bmp|jpe?g|gif|png|webp)$/i,
         include: resolve(__dirname, './src/content/media/general'),
-        use: [
-          {
-            loader: 'file-loader',
-            options: config.module.rules[1].oneOf[1].options,
-          },
-        ],
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: BASE64_MAX_SIZE } },
       },
       {
-        test: /\.(bmp|jpe?g|gif|png)$/i,
+        test: /\.(bmp|jpe?g|gif|png|webp)$/i,
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: BASE64_MAX_SIZE } },
         use: [
-          {
-            loader: 'file-loader',
-            options: config.module.rules[1].oneOf[1].options,
-          },
           {
             loader: 'webpack-image-resize-loader',
             options: {
@@ -73,13 +65,13 @@ module.exports = function override(config) {
             },
           },
         ],
-      }
+      },
     );
   }
 
   // disable manifest
   config.plugins = config.plugins.filter(
-    plugin => plugin.constructor.name !== 'ManifestPlugin'
+    plugin => plugin?.options?.fileName !== 'asset-manifest.json'
   );
 
   return config;
